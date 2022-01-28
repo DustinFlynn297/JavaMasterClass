5086fc7708eb6f9e697642c6de02a8350d283f36
Dustin Flynn <flynnd297@gmail.com>
2022-01-27 18:47:42 -0600

    Created two more number variables and a third variable to add all previous varaiables.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,7 +3,11 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Dustin!");
 
-        int myFirstNumber = 5;
-        System.out.println(myFirstNumber);
+        int myFirstNumber = (10 + 5) + (2 * 10);
+
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 3;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
     }
 }
