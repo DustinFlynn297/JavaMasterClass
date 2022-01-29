af9922cde73e32d2aecb2ae1e9c592d2fd579df4
Dustin Flynn <flynnd297@gmail.com>
2022-01-29 12:32:31 -0600

    Strings can store numbers, but not as integers, it stored as a string data type and therefore adding two strings that contain numbers will not result in addition, just the two numbers appending one behind the other.

--- Strings/src/com/company/Main.java
+++ Strings/src/com/company/Main.java
@@ -7,5 +7,12 @@ public class Main {
         System.out.println("myString is equal to " + myString);
         myString = myString + ", and this is more.";
         System.out.println("myString is equal to " + myString);
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("LastString is equal to " + lastString);
     }
 }
