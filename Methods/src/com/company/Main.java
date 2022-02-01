f6eeb3b71801ce0a548f2097753c2bdf2babec82
Dustin Flynn <flynnd297@gmail.com>
2022-01-31 20:23:12 -0600

    Created a separate method to do the calculation and then passed the arguments to the method.

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -3,23 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+	    calculateScore(true, 800, 5, 100);
+
+        calculateScore(true, 10000, 8, 200);
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if(gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
-            System.out.println("Your final score was  " + finalScore);
-        }
-
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-
-        if(gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
             System.out.println("Your final score was  " + finalScore);
         }
     }
