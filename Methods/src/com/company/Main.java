f687adb9f087f655d85ba817b7cc62f15e0270d6
Dustin Flynn <flynnd297@gmail.com>
2022-02-01 20:18:41 -0600

    Changed the calculateScore method from void to int data type and added a return finalScore to the code so that it actually returns data.

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -14,12 +14,15 @@ public class Main {
         calculateScore(true, 10000, 8, 200);
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if(gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
             System.out.println("Your final score was  " + finalScore);
+            return finalScore;
         }
+
+        return -1;
     }
 }
