dd0b135d033383883769faab46544fd3ae40ceff
Dustin Flynn <flynnd297@gmail.com>
2022-02-01 20:36:42 -0600

    Method challenge to create two methods and invoke them.

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -9,9 +9,21 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-	    calculateScore(gameOver, score, levelCompleted, bonus);
+	    int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was  " + highScore);
 
-        calculateScore(true, 10000, 8, 200);
+        highScore = calculateScore(true, 10000, 8, 200);
+        System.out.println("Your final score was  " + highScore);
+
+        int position = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tim", position);
+
+        position = calculateHighScorePosition(900);
+        displayHighScorePosition("Brian", position);
+
+        position = calculateHighScorePosition(400);
+        displayHighScorePosition("Percy", position);
+
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -19,10 +31,25 @@ public class Main {
         if(gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score was  " + finalScore);
             return finalScore;
         }
 
         return -1;
+    }
+
+    public static void displayHighScorePosition(String playerOne, int position){
+        System.out.println(playerOne + " managed to get into position " + position + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500 && playerScore < 1000) {
+            return 2;
+        } else if (playerScore >= 100 && playerScore < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 }
