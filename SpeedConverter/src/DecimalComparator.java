df9ae0be3ff6ff5d34294a369cb04b28d12183ac
Dustin Flynn <flynnd297@gmail.com>
2022-02-11 22:26:28 -0600

    Created a DecimalComparator class that will multiply two decimals by 1000 to compare if they are equal.

---
+++ SpeedConverter/src/DecimalComparator.java
@@ -0,0 +1,8 @@
+public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double a, double b) {
+        a = (int) (a * 1000);
+        b = (int) (b * 1000);
+
+        return a == b;
+    }
+}
